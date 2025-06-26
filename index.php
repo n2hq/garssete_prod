ee84d8de3ecb8fba0b15551e16756242d5bdfe13
n2hq <n2hq.co@gmail.com>
2025-06-26 17:51:49 +0100

    update commit

--- index.php
+++ index.php
@@ -2,10 +2,26 @@
     <head>
         <title>Garssete - Home</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            .flex_container{
+                font-size:17px; 
+                display:flex; 
+                justify-content:center; 
+                align-items:center; 
+                height:100vh;
+            }
+
+            .centered_div{
+                width:auto; 
+                height:100px; 
+                text-align:center; 
+                line-height:100px
+            }
+        </style>
     </head>
     <body>
-    <div style="font-size:17px; display:flex; justify-content:center; align-items:center; height:100vh;">
-        <div style="width:200px; height:100px; text-align:center; line-height:100px;">
+    <div class="flex_container">
+        <div class="centered_div">
             Hang on for a minute and refresh...
         </div>
     </div>

77ca1cbb897c2108593de995450285735cb4b44d
n2hq <n2hq.co@gmail.com>
2025-06-26 17:47:53 +0100

    update commit

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <html>
     <head>
-
+        <title>Garssete - Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-    <div style="font-size:13px; display:flex; justify-content:center; align-items:center; height:100vh;">
+    <div style="font-size:17px; display:flex; justify-content:center; align-items:center; height:100vh;">
         <div style="width:200px; height:100px; text-align:center; line-height:100px;">
-            Hang on. We are looking into it...
+            Hang on for a minute and refresh...
         </div>
     </div>
 

43df802fc266aa0167b84481adc20f19e6cfed95
n2hq <n2hq.co@gmail.com>
2025-06-26 17:44:55 +0100

    update commit

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Maintenance...";
-?>
+<html>
+    <head>
+
+    </head>
+    <body>
+    <div style="font-size:13px; display:flex; justify-content:center; align-items:center; height:100vh;">
+        <div style="width:200px; height:100px; text-align:center; line-height:100px;">
+            Hang on. We are looking into it...
+        </div>
+    </div>
+
+    </body>
+</html>

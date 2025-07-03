e39ba7a31ab65bee657425c54db7d4eeffbd4b62
n2hq <n2hq.co@gmail.com>
2025-07-03 21:09:12 +0100

    Prod: update commit

--- build/client/assets/_404-BsRiWsBc.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-0DLF9kdB.js";import{G as t}from"./GenericNav-DxnfAy1K.js";import"./components-DN9bEK-g.js";import"./index-DhkpV1_a.js";import"./lib-Y_mKruQP.js";import"./MobileNav-DuMnp-dh.js";import"./AuthContext-FFpxFTZw.js";import"./index-J1ACD94P.js";import"./index-DlU_14A9.js";import"./WhiteLogo-09buATrq.js";function p(){return e.jsxs("div",{children:[e.jsx(t,{}),e.jsx("div",{className:`min-h-screen flex items-center justify-center 
-        text-center px-4 text-black fixed top-0 w-full`,children:e.jsxs("div",{className:`flex place-content-center place-items-center gap-3
-                h-[50px]`,children:[e.jsx("div",{className:`text-2xl font-bold text-red-600 h-full
-                    flex place-items-center`,children:"404"}),e.jsx("div",{className:"h-full w-[2px] border-r"}),e.jsx("div",{className:`text-black text-[14px]
-                    h-full flex place-items-center`,children:"This page could not be found."})]})})]})}export{p as default};

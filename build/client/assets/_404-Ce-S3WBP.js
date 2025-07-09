9e181a44e0b75662a06b0a6d31791a595bc2b891
n2hq <n2hq.co@gmail.com>
2025-07-09 23:52:52 +0100

    Prod: update commit

--- build/client/assets/_404-Ce-S3WBP.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-0DLF9kdB.js";import{G as t}from"./GenericNav-CfdPIzQi.js";import"./components-DN9bEK-g.js";import"./index-E0LPY0kz.js";import"./lib-BYiK7A-4.js";import"./MobileNav-pMqaod1J.js";import"./AuthContext-CTZjTDpv.js";import"./index-BhlUQ65K.js";import"./index-DX6LMNYi.js";import"./WhiteLogo-DYBGE8Y-.js";function p(){return e.jsxs("div",{children:[e.jsx(t,{}),e.jsx("div",{className:`min-h-screen flex items-center justify-center 
-        text-center px-4 text-black fixed top-0 w-full`,children:e.jsxs("div",{className:`flex place-content-center place-items-center gap-3
-                h-[50px]`,children:[e.jsx("div",{className:`text-2xl font-bold text-red-600 h-full
-                    flex place-items-center`,children:"404"}),e.jsx("div",{className:"h-full w-[2px] border-r"}),e.jsx("div",{className:`text-black text-[14px]
-                    h-full flex place-items-center`,children:"This page could not be found."})]})})]})}export{p as default};

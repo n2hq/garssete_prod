de90502682629e336e74b5ff61dd7580fe40ca77
n2hq <n2hq.co@gmail.com>
2025-07-11 10:51:07 +0100

    Prod: update commit

--- app/routes/api/listing/search.tsx
+++ app/routes/api/listing/search.tsx
@@ -76,7 +76,7 @@ export const loader: LoaderFunction = async ({ request, params }) => {
                 GROUP BY 
                 d.gid, d.title, d.short_description, d.phone, d.category, 
                 d.established, d.address_one, d.address_two, d.website, 
-                st.name, ci.name, b.image_url
+                co.name, st.name, ci.name, b.image_url
                 ORDER BY d.date_created ASC
                 LIMIT 50;`)
         }
--- build/server/index.js
+++ build/server/index.js
@@ -11007,7 +11007,7 @@ const loader$p = async ({ request, params }) => {
                 GROUP BY 
                 d.gid, d.title, d.short_description, d.phone, d.category, 
                 d.established, d.address_one, d.address_two, d.website, 
-                st.name, ci.name, b.image_url
+                co.name, st.name, ci.name, b.image_url
                 ORDER BY d.date_created ASC
                 LIMIT 50;`);
     }

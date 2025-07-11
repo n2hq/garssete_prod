82b48cbfdcf85cdf9075a34de6144379f93d2bb1
n2hq <n2hq.co@gmail.com>
2025-07-11 13:29:06 +0100

    Prod: update commit

--- app/routes/api/listing/search.tsx
+++ app/routes/api/listing/search.tsx
@@ -64,7 +64,46 @@ export const loader: LoaderFunction = async ({ request, params }) => {
                 ORDER BY d.date_created ASC
                 LIMIT 50`
 
-        let rawdata: any = await query(sqlc, [criteria, criteria, criteria])
+        let rawdata: any = await query(`SELECT
+                d.id,
+                d.gid,
+                d.title,
+                d.short_description,
+                d.phone,
+                d.category,
+                d.established,
+                d.address_one,
+                d.address_two,
+                d.website,
+                d.date_created,
+                (SELECT name FROM tbl_country co WHERE co.iso2 = d.country_code LIMIT 1) AS country_name,
+                (SELECT name FROM tbl_state st WHERE st.iso2 = d.state_code LIMIT 1) AS state_name,
+                (SELECT name FROM tbl_city ci WHERE ci.id = d.city_id LIMIT 1) AS city_name,
+                b.image_url,
+                r.average_rating,
+                r.total_reviews
+
+                FROM tbl_dir d
+
+                LEFT JOIN tbl_business_profile_image b ON d.gid = b.business_guid
+
+                LEFT JOIN (
+                    SELECT 
+                        business_guid,
+                        ROUND(AVG(rating), 1) AS average_rating,
+                        COUNT(*) AS total_reviews
+                    FROM tbl_rating
+                    GROUP BY business_guid
+                ) r ON d.gid = r.business_guid
+
+                WHERE
+                (d.title RLIKE ?
+                OR d.short_description RLIKE ?
+                OR d.category RLIKE ?)
+                AND
+                d.active_status = true
+                ORDER BY d.date_created ASC
+                LIMIT 50`, [criteria, criteria, criteria])
 
         if (criteria === "" || criteria === null || criteria === undefined) {
             rawdata = await query(`SELECT

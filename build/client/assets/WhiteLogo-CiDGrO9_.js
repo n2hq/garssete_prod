130d1f1f30ffed214429e531eae95bc074842317
n2hq <n2hq.co@gmail.com>
2025-07-17 13:20:44 +0100

    Prod: update commit

--- build/client/assets/WhiteLogo-CiDGrO9_.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-0DLF9kdB.js";import{g as o}from"./lib-D3V_1lJO.js";import{L as r}from"./components-DN9bEK-g.js";const p="mb-8 w-[100%]",l="text-black ml-1.5 mb-2 font-semibold",i=`w-full text-gray-400 
-mt-[-2px] text-[12px] font-normal leading-[1.2em]`,x=`border-[1px] border-gray-500/20 w-full px-3 py-2 rounded 
- bg-gray-100`,m=`w-full mt-0 text-[13px] bg-red-100/50 
-py-2 px-3 rounded-sm flex place-items-center gap-1`,c="w-full mx-auto mt-10 flex flex-col items-center",d="mb-8 w-[100%] ",f=`border-[1px] border-gray-500/30 w-full px-3 py-2 
-rounded bg-gray-100/40 h-[250px] text-[14px]`,s="",g=()=>t.jsx(r,{to:"/",children:t.jsx("div",{className:` text-[24px]
-                cursor-pointer tracking-tighter relative
-                top-[-1px] ${s} font-poppins font-bold`,children:o()})});export{g as W,l as a,x as b,i as c,m as d,d as e,c as f,p as i,f as t,s as w};

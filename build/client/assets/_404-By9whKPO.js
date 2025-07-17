5836fecb0521e994f1934945a3461d2f3e56915d
n2hq <n2hq.co@gmail.com>
2025-07-17 11:14:27 +0100

    Prod: update commit

---
+++ build/client/assets/_404-By9whKPO.js
@@ -0,0 +1,5 @@
+import{j as e}from"./jsx-runtime-0DLF9kdB.js";import{G as t}from"./GenericNav-Betr_69w.js";import"./components-DN9bEK-g.js";import"./index-Dvqxy3jw.js";import"./lib-BlTc6cd4.js";import"./MobileNav-C8eWAniV.js";import"./AuthContext-DToC6GQg.js";import"./index-DcGkkN9N.js";import"./index-Ccvr4QYx.js";import"./WhiteLogo-BjWvq97Q.js";function p(){return e.jsxs("div",{children:[e.jsx(t,{}),e.jsx("div",{className:`min-h-screen flex items-center justify-center 
+        text-center px-4 text-black fixed top-0 w-full`,children:e.jsxs("div",{className:`flex place-content-center place-items-center gap-3
+                h-[50px]`,children:[e.jsx("div",{className:`text-2xl font-bold text-red-600 h-full
+                    flex place-items-center`,children:"404"}),e.jsx("div",{className:"h-full w-[2px] border-r"}),e.jsx("div",{className:`text-black text-[14px]
+                    h-full flex place-items-center`,children:"This page could not be found."})]})})]})}export{p as default};
--- build/client/assets/_404-DwUTobDw.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-0DLF9kdB.js";import{G as t}from"./GenericNav-BdqNNIUv.js";import"./components-DN9bEK-g.js";import"./index-CWCfG8zD.js";import"./lib-D3V_1lJO.js";import"./MobileNav-Dy3u7yT7.js";import"./AuthContext-Bx4JNZJ0.js";import"./index-DGu3pfy-.js";import"./index-D9MLYJla.js";import"./WhiteLogo-CiDGrO9_.js";function p(){return e.jsxs("div",{children:[e.jsx(t,{}),e.jsx("div",{className:`min-h-screen flex items-center justify-center 
-        text-center px-4 text-black fixed top-0 w-full`,children:e.jsxs("div",{className:`flex place-content-center place-items-center gap-3
-                h-[50px]`,children:[e.jsx("div",{className:`text-2xl font-bold text-red-600 h-full
-                    flex place-items-center`,children:"404"}),e.jsx("div",{className:"h-full w-[2px] border-r"}),e.jsx("div",{className:`text-black text-[14px]
-                    h-full flex place-items-center`,children:"This page could not be found."})]})})]})}export{p as default};

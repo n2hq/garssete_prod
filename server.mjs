bec1623b06deb52812d8855ab31bb2d881b203f6
n2hq <n2hq.co@gmail.com>
2025-07-17 18:33:05 +0100

    Test: update commit

--- server.mjs
+++ server.mjs
@@ -1,7 +1,21 @@
+import dotenv from "dotenv";
+
+// Dynamically load the right env file based on NODE_ENV
+const envFileMap = {
+  test: "./env/.env.test",
+  production: "./env/.env.prod",
+  development: "./env/.env.dev"
+};
+
+dotenv.config({
+  path: envFileMap[process.env.NODE_ENV] || "./env/.env.prod"
+});
+
 import { createRequestHandler } from "@remix-run/express";
 import compression from "compression";
 import express from "express";
 import morgan from "morgan";
+
 
 const viteDevServer =
   process.env.NODE_ENV === "production"
@@ -19,7 +33,8 @@ const remixHandler = createRequestHandler({
 });
 
 const app = express();
-const PORT = 3393;
+//const PORT = 3393;
+const port = process.env.PORT || process.env.VITE_PORT || 3000;
 
 app.use(compression());
 
@@ -46,7 +61,9 @@ app.use(morgan("tiny"));
 // handle SSR requests
 app.all("*", remixHandler);
 
-const port = process.env.VITE_PORT || PORT;
+console.log("🔧 Loaded ENV:", process.env.VITE_PORT, process.env.VITE_SITENAME);
+
+//const port = process.env.VITE_PORT || PORT;
 app.listen(port, () =>
   console.log(`Express server listening at http://localhost:${port}`)
 );

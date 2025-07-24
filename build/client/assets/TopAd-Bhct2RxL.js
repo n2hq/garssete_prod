c7a2a84ede0a8523273d5e4f956c42722aecfe70
n2hq <n2hq.co@gmail.com>
2025-07-24 11:40:21 +0100

    Prod: update commit

---
+++ build/client/assets/TopAd-Bhct2RxL.js
@@ -0,0 +1,4 @@
+import{j as t}from"./jsx-runtime-0DLF9kdB.js";import{r as e}from"./components-DN9bEK-g.js";const d=[{id:1,url:"/",label:"Home"},{id:2,url:"/web/search",label:"Search"},{id:3,url:"/web/search?q=hotels",label:"Hotels"},{id:4,url:"/web/search?q=travel",label:"Travel"},{id:5,url:"/web/search?q=real estate",label:"Real Estate"},{id:6,url:"/web/search?q=services",label:"Services"}],s={adslot:"1234567890"},a={adslot:"1234567890"};function n(){const[l,r]=e.useState(!1);return e.useEffect(()=>{},[]),l?t.jsxs("div",{className:`max-w-[1000px] min-h-[90px] bg-blue-50
+                mx-auto w-full mt-4 flex place-items-center 
+                place-content-center font-light text-[14px]
+                `,children:["Ads by google",s.adslot!==a.adslot]}):null}export{n as T,d as n};

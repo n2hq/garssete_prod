01b88763016f3d679aa021350827fb2ec9c7cfbf
n2hq <n2hq.co@gmail.com>
2025-07-27 01:11:45 +0100

    Prod: update commit

--- app/routes/web/reset_password/assets/ResetPasswordBody.tsx
+++ app/routes/web/reset_password/assets/ResetPasswordBody.tsx
@@ -5,8 +5,8 @@ import ResetPasswordForm from './ResetPasswordForm'
 const ResetPasswordBody = () => {
     return (
         <div className={`bg-white w-full 
-        md:bg-[url('/images/office2.jpg')]
-        bg-cover bg-center min-h-screen flex place-content-center relative`}>
+        md:bg-[url('https://www.theglasswarehouse.co.uk/wp-content/uploads/2025/07/Modern-open-plan-office-showing-both-glass-partition-walls-and-traditional-drywall-meeting-rooms.jpg')]
+        bg-cover bg-center min-h-screen flex place-content-center `}>
 
             <ResetPasswordForm />
         </div>

100053e44dc82ebce1a6732a589afcd7f2d8fe35
n2hq <n2hq.co@gmail.com>
2025-07-28 10:09:34 +0100

    Prod: update commit

---
+++ build/client/assets/VerticalHeight-Kp1WMsWI.js
@@ -0,0 +1,1 @@
+import{j as s}from"./jsx-runtime-0DLF9kdB.js";import{S as l}from"./SearchNavbar-5qlTiyNU.js";import{a as r}from"./lib-qiNLtO-W.js";const m=({children:a})=>s.jsxs("div",{className:"h-full w-full",children:[s.jsx(l,{}),s.jsx("main",{className:`h-full flex w-full flex-col mt-[${r.NAVBAR_HEIGHT}px]`,children:a})]}),i=()=>s.jsx("div",{className:"h-20"});export{m as L,i as V};

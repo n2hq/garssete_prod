85b996059824779bd037303f7a24087693df17e4
n2hq <n2hq.co@gmail.com>
2025-08-03 13:49:59 +0100

    Test: update commit

--- app/routes/_404.tsx
+++ app/routes/_404.tsx
@@ -14,15 +14,22 @@ export default function NotFound() {
             <div className={`min-h-screen flex items-center justify-center 
         text-center px-4 text-black fixed top-0 w-full`}>
                 <div className={`flex place-content-center place-items-center gap-3
-                h-[50px]`}>
+                h-[40px]`}>
                     <div className={`text-2xl font-bold text-red-600 h-full
                     flex place-items-center`}>
                         404
                     </div>
                     <div className={`h-full w-[2px] border-r`}></div>
                     <div className={`text-black text-[14px]
-                    h-full flex place-items-center`}>
-                        This page could not be found.
+                    h-full flex place-items-center space-x-2`}>
+                        <span>
+                            Resource not found.
+                        </span>
+                        <button
+                            onClick={() => { window.history.back() }}
+                            className={`underline`}>
+                            Go back
+                        </button>
                     </div>
                 </div>
             </div>
--- app/routes/listing/assets/ResourceNotFound.tsx
+++ app/routes/listing/assets/ResourceNotFound.tsx
@@ -3,13 +3,27 @@ import React from 'react'
 const ResourceNotFound = () => {
     return (
         <div className={`flex place-content-center place-items-center w-full min-h-[700px] text-[13px] flex-col space-y-4`}>
-            <div className={`font-bold text-[16px]`}>
-                Resource Not Found</div>
-            <button
-                onClick={() => { window.history.back(-1) }}
-                className={`border-blue-500 border px-5 py-[3px]
-                rounded hover:bg-blue-100`}>
-                Go back</button>
+
+            <div className={`flex place-content-center place-items-center gap-3
+                h-[40px]`}>
+                <div className={`text-2xl font-bold text-red-600 h-full
+                    flex place-items-center`}>
+                    404
+                </div>
+                <div className={`h-full w-[2px] border-r`}></div>
+                <div className={`text-black text-[14px]
+                    h-full flex place-items-center space-x-2`}>
+                    <span>
+                        Resource not found.
+                    </span>
+                    <button
+                        onClick={() => { window.history.back() }}
+                        className={`underline`}>
+                        Go back
+                    </button>
+                </div>
+            </div>
+
         </div>
     )
 }

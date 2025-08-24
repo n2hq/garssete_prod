788983efcd1c8707080e49c77c892d0cf7fd6405
n2hq <n2hq.co@gmail.com>
2025-08-24 15:52:27 +0100

    Prod: update commit

--- app/routes/listing/assets/LocationWithHours.tsx
+++ app/routes/listing/assets/LocationWithHours.tsx
@@ -127,7 +127,7 @@ const LocationWithHours = ({ listing, operatingHoursStatus }: any) => {
     useEffect(() => {
         let getListingAddress = async (listing: any) => {
             let address = listing?.title
-            address += listing?.address_one ? listing?.address_one : ''
+            address += listing?.address_one ? ', ' + listing?.address_one : ''
             address += listing?.address_two ? ', ' + listing?.address_two : ''
             address += listing?.city_name ? ', ' + listing?.city_name : ''
             address += listing?.state_name ? ', ' + listing?.state_name : ''

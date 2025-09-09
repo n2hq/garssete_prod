6a3d9f1c1efe701553e657c8bd18d352cdfc9eae
n2hq <n2hq.co@gmail.com>
2025-09-09 22:15:00 +0100

    Prod: update commit

--- app/routes/listing/index.tsx
+++ app/routes/listing/index.tsx
@@ -5,7 +5,7 @@ import SearchHead from '~/components/content/SearchHead'
 import ResponsiveNav from '~/components/header/lite/ResponsiveNav'
 import { GalleryProvider } from '~/context/GalleryContext'
 import RatingProvider from '~/context/RatingContext'
-import { config, getBusinessGallery, getBusinessProfileImageData, getBusinessVideoGallery, getOperatingHours, getPage, getProductGallery, getRatingsReviews, getSearch, getVideoGallery } from '~/lib/lib'
+import { config, getBusinessGallery, getBusinessProfileImageData, getBusinessVideoGallery, getOperatingHours, getPage, getProductGallery, getRatingsReviews, getSearch, getVideoGallery, logError } from '~/lib/lib'
 import BusinessLayout from './assets/BusinessLayout'
 import Footer from '~/components/footer/Footer'
 import Related from './assets/Related'
@@ -63,56 +63,62 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 }
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
-    const listing = data?.listing
-    const profileImageData = data?.profileImageData
+    try {
+        const listing = data?.listing
+        const profileImageData = data?.profileImageData
 
-    let profileImageLink = ""
+        let profileImageLink = ""
 
-    if (profileImageData?.image_url === "" || profileImageData?.image_url === null || profileImageData?.image_url === undefined) {
-        profileImageLink = `${config.BASE_URL}/images/abstract_placeholder.jpg`
-    } else {
-        profileImageLink = config.IMG_BASE_URL + profileImageData?.image_url
-    }
-
-    const mimetype = profileImageData?.mimetype
-
-
-    const getKeyWords = (texts: string | null): string[] | null => {
-        if (texts === null || texts === undefined || texts === "") {
-            return null
+        if (profileImageData?.image_url === "" || profileImageData?.image_url === null || profileImageData?.image_url === undefined) {
+            profileImageLink = `${config.BASE_URL}/images/abstract_placeholder.jpg`
+        } else {
+            profileImageLink = config.IMG_BASE_URL + profileImageData?.image_url
         }
-        const cleaned = texts.replace(/&/g, ',');
 
-        // Step 2: Split by comma and trim each part
-        const arr = cleaned
-            .split(',')
-            .map(item => item.trim())
-            .filter(item => item.length > 0);
+        const mimetype = profileImageData?.mimetype
 
-        return arr
+
+        const getKeyWords = (texts: string | null): string[] | null => {
+            if (texts === null || texts === undefined || texts === "") {
+                return null
+            }
+            const cleaned = texts.replace(/&/g, ',');
+
+            // Step 2: Split by comma and trim each part
+            const arr = cleaned
+                ?.split(',')
+                ?.map(item => item.trim())
+                ?.filter(item => item.length > 0);
+
+            return arr
+        }
+
+        const keywords: string[] | null = getKeyWords(listing?.business_phrases || null)
+
+        return [
+            { title: listing?.title || "Garssete Inc." },
+            { name: "description", content: listing?.short_description },
+            { name: "keywords", content: keywords },
+            { property: "fb:app_id", content: "1325393508603168" },
+            { property: "og:url", content: listing?.website || "https://garssete.com" },
+            { property: "og:type", content: "website" },
+            { property: "og:title", content: listing?.title || "Garssete.com" },
+            { property: "og:description", content: listing?.short_description },
+            { property: "og:image", content: profileImageLink },
+            { property: "og:image:secure_url", content: profileImageLink },
+            { property: "og:image:type", content: mimetype },
+            { property: "og:image:width", content: "200" },
+            { property: "og:image:alt", content: listing?.title || "Garssete" },
+            { name: "twitter:creator", content: "garssete" },
+            { name: "twitter:card", content: "summary_large_image" },
+            { name: "twitter:title", content: listing?.title || "Garssete Directory Listing" },
+            { name: "twitter:description", content: listing?.short_description },
+        ];
+    } catch (e: any) {
+        logError(e)
     }
 
-    const keywords: string[] | null = getKeyWords(listing?.business_phrases || null)
-
-    return [
-        { title: listing?.title || "Garssete Inc." },
-        { name: "description", content: listing?.short_description },
-        { name: "keywords", content: keywords },
-        { property: "fb:app_id", content: "1325393508603168" },
-        { property: "og:url", content: listing?.website || "https://garssete.com" },
-        { property: "og:type", content: "website" },
-        { property: "og:title", content: listing?.title || "Garssete.com" },
-        { property: "og:description", content: listing?.short_description },
-        { property: "og:image", content: profileImageLink },
-        { property: "og:image:secure_url", content: profileImageLink },
-        { property: "og:image:type", content: mimetype },
-        { property: "og:image:width", content: "200" },
-        { property: "og:image:alt", content: listing?.title || "Garssete" },
-        { name: "twitter:creator", content: "garssete" },
-        { name: "twitter:card", content: "summary_large_image" },
-        { name: "twitter:title", content: listing?.title || "Garssete Directory Listing" },
-        { name: "twitter:description", content: listing?.short_description },
-    ];
+    return []
 };
 
 const index = () => {

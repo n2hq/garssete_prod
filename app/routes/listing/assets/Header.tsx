c5948dadcf3f3eaa8cb3c2ddc1a7f1a3211b09d1
n2hq <n2hq.co@gmail.com>
2025-09-18 09:45:29 +0100

    Prod: update commit

--- app/routes/listing/assets/Header.tsx
+++ app/routes/listing/assets/Header.tsx
@@ -13,65 +13,71 @@ const Header = ({ listing, profileImageData, operatingHoursStatus, ratingsData }
     }, [profileImageData])
 
     return (
-        <div className={`flex gap-2 mt-2 w-full border-t-4 border-blue-700 pt-2`}>
-            <div className={`bg-white w-[95px] min-w-[95px] h-[95px] rounded-none relative overflow-hidden hidden md:hidden border border-gray-200  shadow-sm`}>
-                <img
-                    src={profileImg}
-                    alt=""
-                    className={` object-cover w-full h-full border-[1px]  border-gray-200`}
-                />
+        <div>
+            <div className={`h-[5px] bg-gradient-to-r from-black to-blue-500 w-full mt-2 `}>
+
             </div>
-            <div className={`w-full`}>
-                <div className={`text-[19px] md:text-[24px]
+            <div className={`flex gap-2 mt-2 w-full `}>
+
+                <div className={`bg-white w-[95px] min-w-[95px] h-[95px] rounded-none relative overflow-hidden hidden md:hidden border border-gray-200  shadow-sm`}>
+                    <img
+                        src={profileImg}
+                        alt=""
+                        className={` object-cover w-full h-full border-[1px]  border-gray-200`}
+                    />
+                </div>
+                <div className={`w-full`}>
+                    <div className={`text-[19px] md:text-[24px]
                 font-bold  leading-[1.2em] tracking-tight font-poppins`}>
-                    {listing?.title}
+                        {listing?.title}
 
-                </div>
+                    </div>
 
-                <div>
+                    <div>
 
-                    <FormattedAddress listing={listing} />
-                </div>
+                        <FormattedAddress listing={listing} />
+                    </div>
 
-                <div className={` leading-[1.3em] space-x-1 mt-1`}>
-                    {
-                        operatingHoursStatus !== undefined &&
-                        <div className={` leading-[1.2em]`}>
-                            {
-                                operatingHoursStatus.openStatus === "selected_hours" ?
-                                    <div>
-                                        {operatingHoursStatus.todayHoursFormatted}
-                                    </div> :
-                                    <div>
-                                        {
-                                            operatingHoursStatus.openStatus === "always_open" &&
-                                            <div key={'1'} className={`bg-green-700 px-1 rounded-sm w-fit text-white pt-[2px] pb-[4px]`}>
-                                                Always Open
-                                            </div>
-                                        }
-                                        {
-                                            operatingHoursStatus.openStatus === "permanently_closed" &&
-                                            <div key={'2'} className={`bg-red-700 px-1 rounded-sm w-fit text-white pt-[2px] pb-[4px]`}>
-                                                Permantently Closed
-                                            </div>
-                                        }
-                                        {
-                                            operatingHoursStatus.openStatus === "temporarily_closed" &&
-                                            <div key={'3'} className={`bg-orange-400 px-1 rounded-sm w-fit text-white pt-[2px] pb-[4px]`}>
-                                                Temporarily Closed
-                                            </div>
-                                        }
-                                    </div>
-                            }
+                    <div className={` leading-[1.3em] space-x-1 mt-1`}>
+                        {
+                            operatingHoursStatus !== undefined &&
+                            <div className={` leading-[1.2em]`}>
+                                {
+                                    operatingHoursStatus.openStatus === "selected_hours" ?
+                                        <div>
+                                            {operatingHoursStatus.todayHoursFormatted}
+                                        </div> :
+                                        <div>
+                                            {
+                                                operatingHoursStatus.openStatus === "always_open" &&
+                                                <div key={'1'} className={`bg-green-700 px-1 rounded-sm w-fit text-white pt-[2px] pb-[4px]`}>
+                                                    Always Open
+                                                </div>
+                                            }
+                                            {
+                                                operatingHoursStatus.openStatus === "permanently_closed" &&
+                                                <div key={'2'} className={`bg-red-700 px-1 rounded-sm w-fit text-white pt-[2px] pb-[4px]`}>
+                                                    Permantently Closed
+                                                </div>
+                                            }
+                                            {
+                                                operatingHoursStatus.openStatus === "temporarily_closed" &&
+                                                <div key={'3'} className={`bg-orange-400 px-1 rounded-sm w-fit text-white pt-[2px] pb-[4px]`}>
+                                                    Temporarily Closed
+                                                </div>
+                                            }
+                                        </div>
+                                }
 
-                            <div className={` w-full mx-auto mt-1.5`}>
-                                {operatingHoursStatus?.localTimeText}
+                                <div className={` w-full mx-auto mt-1.5`}>
+                                    {operatingHoursStatus?.localTimeText}
+                                </div>
                             </div>
-                        </div>
 
-                    }
+                        }
 
 
+                    </div>
                 </div>
             </div>
         </div>

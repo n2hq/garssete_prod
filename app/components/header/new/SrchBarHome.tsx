715040c36dcab2cfe741952bd468f9f9c118905b
n2hq <n2hq.co@gmail.com>
2025-09-25 09:04:04 +0100

    Prod: update commit

--- app/components/header/new/SrchBarHome.tsx
+++ app/components/header/new/SrchBarHome.tsx
@@ -48,7 +48,7 @@ const SrchBarHome = () => {
 
     return (
         <div>
-            <div className={` bg-[#520dc1] text-white  fixed top-0 left-0 w-full z-[1000] shadow-sm`} style={{ height: `${appConfig.NAVBAR_HEIGHT}px` }}>
+            <div className={` bg-[blue] text-white  fixed top-0 left-0 w-full z-[1000] shadow-sm`} style={{ height: `${appConfig.NAVBAR_HEIGHT}px` }}>
 
                 <MobileNav
                     showNav={showNav}

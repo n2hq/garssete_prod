ef9bb95ccfdd09827fd9b6abf80e51cb51b370a7
n2hq <n2hq.co@gmail.com>
2025-09-28 20:14:19 +0100

    Prod: update commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -77,7 +77,7 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
       { property: "fb:app_id", content: "1325393508603168" },
       { property: "og:url", content: "https://garssete.com" },
       { property: "og:type", content: "website" },
-      { property: "og:title", content: "Garssete | Business Directory, Explore Listings Around The World" },
+      { property: "og:title", content: "Garssete - Online Business Directory, Explore Listings Around The World" },
       { property: "og:description", content: "Discover and connect with businesses worldwide. Garssete.com helps you explore listings, find services, and grow your network across industries and countries." },
       { property: "og:image", content: `https://edition.garssete.com/images/gsbg.png?v=${randomNo}` },
       { property: "og:image:secure_url", content: `https://edition.garssete.com/images/gsbg.png?v=${randomNo}` },
@@ -86,7 +86,7 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
       { property: "og:image:alt", content: "Garssete" },
       { name: "twitter:creator", content: "garssete" },
       { name: "twitter:card", content: "summary_large_image" },
-      { name: "twitter:title", content: "Garssete | Business Directory, Explore Listings Around The World" },
+      { name: "twitter:title", content: "Garssete - Online Business Directory, Explore Listings Around The World" },
       { name: "twitter:description", content: "Discover and connect with businesses worldwide. Garssete.com helps you explore listings, find services, and grow your network across industries and countries." },
     ];
   } catch (e: any) {
